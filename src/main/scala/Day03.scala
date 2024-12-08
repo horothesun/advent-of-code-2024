2d53cb44d31b7ddc3634ca5a79eab0a7f93b5e23
horothesun <horothesun@gmail.com>
2024-12-08 15:45:16 +0000

    Day 3 part 2

--- src/main/scala/Day03.scala
+++ src/main/scala/Day03.scala
@@ -1,10 +1,14 @@
+import Day03.Op.*
+import Day03.SeparatedElement.*
 import cats.data.NonEmptyList
 import cats.parse.{Parser, Parser0}
 import cats.parse.Numbers.nonNegativeIntString
-import cats.parse.Parser.{char, end, string, until, until0, Error}
+import cats.parse.Parser.{char, end, oneOf, string, until, until0, Error}
 import cats.syntax.all.*
 
 object Day03:
+
+  // part 1
 
   case class Mul(x: Int, y: Int):
     def eval: Int = x * y
@@ -23,3 +27,66 @@ object Day03:
 
   def evalMulsOnly(inputs: List[String]): Either[Error, Int] =
     inputs.traverse(s => mulsOnlyParser.parseAll(s).map(_.foldMap(_.eval))).map(_.sum)
+
+  // part 2
+
+  sealed trait SeparatedElement[A, S]
+  object SeparatedElement:
+    case class Sep[A, S](s: S) extends SeparatedElement[A, S]
+    case class Elem[A, S](a: A) extends SeparatedElement[A, S]
+
+  extension [A, S](l: List[SeparatedElement[A, S]])
+    def splitBySep(start: S): List[(Sep[A, S], List[Elem[A, S]])] =
+      val (acc, last) =
+        l.foldLeft[(List[(Sep[A, S], List[Elem[A, S]])], (Sep[A, S], List[Elem[A, S]]))](
+          (List.empty, (Sep[A, S](start), List.empty))
+        ) { case ((accElemOrSep, (sep, elems)), elemOrSep) =>
+          elemOrSep match
+            case newSep @ Sep(_)   => (accElemOrSep :+ (sep, elems), (newSep, List.empty))
+            case newElem @ Elem(_) => (accElemOrSep, (sep, elems :+ newElem))
+        }
+      acc :+ last
+
+  type Enabler = Do.type | DoNot.type
+
+  sealed trait Op:
+    def toSeparatedElement: SeparatedElement[Multiply, Enabler] = this match
+      case op @ (Do | DoNot)    => Sep(op)
+      case mul @ Multiply(_, _) => Elem(mul)
+
+  object Op:
+
+    case object Do extends Op:
+      def parser: Parser[Do.type] = string("do()").as(Do)
+
+    case object DoNot extends Op:
+      def parser: Parser[DoNot.type] = string("don't()").as(DoNot)
+
+    case class Multiply(x: Int, y: Int) extends Op:
+      def eval: Int = x * y
+    object Multiply:
+      def parser: Parser[Multiply] =
+        val natLt1k = nonNegativeIntString.collect { case s if 1 <= s.length && s.length <= 3 => s.toInt }
+        val natLt1kPair = (
+          char('(') *> natLt1k.repSep(sep = char(',')) <* char(')')
+        ).collect { case NonEmptyList(x, y :: Nil) => (x, y) }
+        string("mul") *> natLt1kPair.map(Multiply.apply.tupled)
+
+    def parser: Parser[Op] = oneOf(List(Do.parser, DoNot.parser, Multiply.parser))
+
+  def group(ops: List[Op]): List[(Enabler, List[Multiply])] =
+    ops
+      .map(_.toSeparatedElement)
+      .splitBySep(start = Do)
+      .map { case (Sep(sep), elems) => (sep, elems.map(_.a)) }
+
+  def evalEnabledMulsOnlyFromOps(ops: List[Op]): Int =
+    group(ops).collect { case (Do, muls) => muls }.flatten.foldMap(_.eval)
+
+  val opsParser: Parser0[List[Op]] =
+    val opRep = Op.parser.backtrack.rep.map(_.toList)
+    until0(Op.parser) *> opRep.repSep0(sep = until(Op.parser)).map(_.flatten) <* until0(end)
+
+  def evalEnabledMulsOnly(inputs: List[String]): Either[Error, Int] =
+    val contiguousInput = inputs.mkString("")
+    opsParser.parseAll(contiguousInput).map(evalEnabledMulsOnlyFromOps)
--- src/test/scala/Day03Suite.scala
+++ src/test/scala/Day03Suite.scala
@@ -1,26 +1,130 @@
 import Day03.*
+import Day03.Op.*
+import Day03.SeparatedElement.*
 import Day03Suite.*
 import munit.ScalaCheckSuite
+import org.scalacheck.Gen
+import org.scalacheck.Prop.*
 
 class Day03Suite extends ScalaCheckSuite:
+
+  // part 1
 
   test("parsing \"mul(42,1)\""):
     assertEquals(Mul.parser.parse("mul(42,1)"), Right(("", Mul(42, 1))))
 
-  test("parsing Muls only from small input"):
+  test("parsing Muls only from small input 1"):
     assertEquals(
-      mulsOnlyParser.parseAll(smallInput.head),
+      mulsOnlyParser.parseAll(smallInput1.head),
       Right(List(Mul(2, 4), Mul(5, 5), Mul(11, 8), Mul(8, 5)))
     )
 
-  test("eval Muls only from small input returns 161"):
-    assertEquals(evalMulsOnly(smallInput), Right(161))
+  test("eval Muls only from small input 1 returns 161"):
+    assertEquals(evalMulsOnly(smallInput1), Right(161))
 
   test("eval Muls only from big input returns 184_511_516"):
     assertEquals(evalMulsOnly(bigInput), Right(184_511_516))
+
+  // part 2
+
+  test("split by Sep sample"):
+    def elem(i: Int): Elem[Int, String | Char] = Elem(i)
+    def sep(s: String | Char): Sep[Int, String | Char] = Sep(s)
+    val l = List[SeparatedElement[Int, String | Char]](
+      elem(42),
+      sep("hi"),
+      elem(1),
+      sep('x'),
+      elem(2),
+      elem(3),
+      sep("bye"),
+      elem(4)
+    )
+    assertEquals(
+      l.splitBySep(start = "START"),
+      List[(Sep[Int, String | Char], List[Elem[Int, String | Char]])](
+        (sep("START"), List(elem(42))),
+        (sep("hi"), List(elem(1))),
+        (sep('x'), List(elem(2), elem(3))),
+        (sep("bye"), List(elem(4)))
+      )
+    )
+
+  property("split by Sep preserves length"):
+    forAll(
+      Gen.listOf(
+        Gen.oneOf[SeparatedElement[Int, String | Char]](
+          Gen.posNum[Int].map(Elem[Int, String | Char].apply),
+          Gen
+            .oneOf[String | Char](Gen.stringOfN(3, Gen.alphaChar), Gen.alphaChar)
+            .map(Sep[Int, String | Char].apply)
+        )
+      )
+    ) { l =>
+      assertEquals(
+        l.splitBySep(start = "START").map(1 + _._2.length).sum,
+        List("START").length + l.length
+      )
+    }
+
+  test("group Ops sample"):
+    assertEquals(
+      group(
+        List[Op](
+          Multiply(2, 4),
+          DoNot,
+          Multiply(5, 5),
+          Multiply(11, 8),
+          Do,
+          Multiply(8, 5)
+        )
+      ),
+      List(
+        (Do, List(Multiply(2, 4))),
+        (DoNot, List(Multiply(5, 5), Multiply(11, 8))),
+        (Do, List(Multiply(8, 5)))
+      )
+    )
+
+  test("eval enabled multiplications only from Ops sample"):
+    assertEquals(
+      evalEnabledMulsOnlyFromOps(
+        List[Op](
+          Multiply(2, 4),
+          DoNot,
+          Multiply(5, 5),
+          Multiply(11, 8),
+          Do,
+          Multiply(8, 5)
+        )
+      ),
+      expected = 48
+    )
+
+  test("parsing Ops from small input 2"):
+    assertEquals(
+      opsParser.parseAll(smallInput2.head),
+      Right(
+        List(
+          Multiply(2, 4),
+          DoNot,
+          Multiply(5, 5),
+          Multiply(11, 8),
+          Do,
+          Multiply(8, 5)
+        )
+      )
+    )
+
+  test("eval enabled multiplications only from small input 2 returns 48"):
+    assertEquals(evalEnabledMulsOnly(smallInput2), Right(48))
+
+  test("eval enabled multiplications only from big input returns 90_044_227"):
+    assertEquals(evalEnabledMulsOnly(bigInput), Right(90_044_227))
 
 object Day03Suite:
 
   val bigInput: List[String] = getLinesFromFile("src/test/scala/day03_input.txt")
 
-  val smallInput: List[String] = List("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+  val smallInput1: List[String] = List("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
+  val smallInput2: List[String] = List("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
